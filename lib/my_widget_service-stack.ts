f554433a246dc56f67bae67fc060382ec92ab705
Oluwafemi Olasbuomi <olasubomifemi98@gmail.com>
2022-08-12 19:22:36 +0100

    init push

--- lib/my_widget_service-stack.ts
+++ lib/my_widget_service-stack.ts
@@ -1,10 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as widget_service from '../lib/widget_service';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class MyWidgetServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    new widget_service.WidgetService(this, 'Widgets');
 
     // The code that defines your stack goes here
 
